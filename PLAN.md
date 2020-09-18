eba5d47cfc85cf218109c97b3cb830febea9da53
MohiEldin <mohyaldeen0101ahmed@gmail.com>
2020-09-18 22:36:27 +0200

    :sparkles: POST /register & front-end setup

--- PLAN.md
+++ PLAN.md
@@ -1,10 +1,10 @@
 ## Auth Demo
 
 * [ ] Register.
-    * [ ] POST /register.
-        * [ ] validate req body.
-        * [ ] check if user already exists.
-        * [ ] hash the password.
-        * [ ] store user in db.
+    * [x] POST /register.
+        * [x] validate req body.
+        * [x] check if user already exists.
+        * [x] hash the password.
+        * [x] store user in db.
     * [ ] GET /register.
 * [ ] Login.
--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,51 @@
 const epxress = require('express');
 const router = epxress.Router();
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const db = require('monk')('localhost/auth-demo');
 
-router.post('/register', (req, res, next) => {
-    res.json({
-        message: "Registering 🔑"
-    });
+const users = db.get('users');
+
+const schema = Joi.object({
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .required(),
+
+    password: Joi.string()
+        .pattern(new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})"))
+        .required(),
+
+    password_repeat: Joi.ref('password'),
+
+})
+    .with('password', 'password_repeat');
+
+router.post('/register', async (req, res, next) => {
+    try {
+        const value = await schema.validateAsync(req.body);
+        const doc = await users.findOne({ email: value.email });
+        if (!doc) {
+            const password_hash = bcrypt.hashSync(value.password, 10);
+            const user = await users.insert({
+                email: value.email,
+                password: password_hash
+            });
+            res.json(user);
+        } else {
+            throw new Error('User Already Exists !');
+        }
+    }
+    catch (err) {
+        res.json(err.message);
+    }
+});
+
+router.get('/register', (req, res, next) => {
+    res.render('register')
+});
+
+router.get('/login', (req, res, next) => {
+    res.render('login')
 });
 
 module.exports = router;
