df9b2329afc0835597a0ad648f62ef7b16f374a0
MohiEldin <mohyaldeen0101ahmed@gmail.com>
2020-09-19 01:21:53 +0200

    :sparkles: POST /login & login form

--- PLAN.md
+++ PLAN.md
@@ -1,10 +1,17 @@
 ## Auth Demo
 
-* [ ] Register.
+* [x] Register.
     * [x] POST /register.
         * [x] validate req body.
         * [x] check if user already exists.
         * [x] hash the password.
         * [x] store user in db.
-    * [ ] GET /register.
-* [ ] Login.
+    * [x] GET /register.
+* [x] Login.
+    * [x] POST /login.
+        * [x] validate req body.
+        * [x] check if user exists in db.
+        * [x] check if password matches.
+        * [x] generate login token & send it to user.
+    * [x] GET /login.
+    
--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const epxress = require('express');
 const router = epxress.Router();
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const db = require('monk')('localhost/auth-demo');
 
 const users = db.get('users');
@@ -20,6 +21,17 @@ const registerSchema = Joi.object({
 })
     .with('password', 'password_repeat');
 
+const loginSchema = Joi.object({
+    email: Joi.string()
+        .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net'] } })
+        .required(),
+
+    password: Joi.string()
+        .pattern(new RegExp("^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{8,})"))
+        .required(),
+})
+    .with('email', 'password');
+
 router.post('/register', async (req, res, next) => {
     try {
         const value = await registerSchema.validateAsync(req.body);
@@ -30,7 +42,7 @@ router.post('/register', async (req, res, next) => {
                 email: value.email,
                 password: password_hash
             });
-            res.render('login', { email: value.email, password: value.password });
+            res.render('login', { email: '', password: '', err: '' });
         } else {
             throw new Error('User Already Exists !');
         }
@@ -44,8 +56,28 @@ router.get('/register', (req, res, next) => {
     res.render('register', { err: '' });
 });
 
+router.post('/login', async (req, res, next) => {
+    try {
+        const value = await loginSchema.validateAsync(req.body);
+        const doc = await users.findOne({ email: value.email });
+        if (doc) {
+            const match = bcrypt.compareSync(value.password, doc.password);
+            if (match) {
+                const token = jwt.sign({ email: doc.email }, 'key');
+                res.redirect(`/?token=${token}`);
+            } else {
+                throw new Error('Email or password incorrect !');
+            }
+        } else {
+            throw new Error('Email or password incorrect !');
+        }
+    } catch (error) {
+        res.render('login', { email: '', password: '', err: error.message });
+    }
+});
+
 router.get('/login', (req, res, next) => {
-    res.render('login', { email: '', password: '' });
+    res.render('login', { email: '', password: '', err: '' });
 });
 
 module.exports = router;
